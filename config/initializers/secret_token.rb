bbe44e0af9a54115ba0c4c4f9bb4869ffca6428e
Nishant <nishant.g181990@gmail.com>
2013-06-03 22:33:58 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SparshClientRecords::Application.config.secret_token = '5f8f726a5e30244c2c37f30f4ce2e158862a6fafea0f8c52fe4d7fc291f8041c192cebd6b2e1d681e46f34baa835bba1cd009145908bdacd11cd26543580caf1'
